6e54792da069846da6380b4fc4c9f5ec48d6c282
More2237 <noorallyrahmonov2308@gmail.com>
2024-11-24 11:55:41 +0500

    fir

---
+++ boolean11/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using static System.Console;
+
+namespace boolean11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(ReadLine());
+            int b = int.Parse(ReadLine());
+            
+            bool result = a%2 ==0 && b%2 == 0;
+            WriteLine(result);
+        }
+    }
+}
